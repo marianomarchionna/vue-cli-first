681c994dff18a3436da2395b422b9505a2cda873
Mariano <marianomarchionna8@gmail.com>
2021-09-17 17:20:56 +0200

    BONUS

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <img src="../assets/boolean.png" alt="Boolean">
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+      
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    img {
+        height: 500px;
+    }
+</style>
